4a7409939acadcddbdf185d455d5d788608fd7e1
deservs <viniciuscapela6014@gmail.com>
2026-08-24 10:14:20 -0300

    alteração na lógica do ocr.worker para uma melhor encontrada na documentação do github

--- client/src/workers/ocr.worker.ts
+++ client/src/workers/ocr.worker.ts
@@ -1,21 +1,17 @@
-import * as ort from 'onnxruntime-web';
+import { PaddleOCR } from '@paddleocr/paddleocr-js'
 
-// Regra vital: Força o ONNX a buscar os binários WebAssembly na pasta pública,
-// caso contrário, o Vite tentará empacotar tudo no build final e quebrará o sistema.
-ort.env.wasm.wasmPaths = '/wasm/';
 
-self.onmessage = async (event: MessageEvent) => {
-  const { action, imageData } = event.data;
-
-  if (action === 'INICIAR_INFERENCIA') {
-    try {
-      // 1. Aqui entrará a lógica de transformação do ImageData em Tensor
-      // 2. A execução da sessão ONNX
-      // 3. O retorno do texto
-
-      self.postMessage({ status: 'sucesso', texto: 'Texto extraído do ONNX' });
-    } catch (erro) {
-      self.postMessage({ status: 'erro', mensagem: String(erro) });
+const iniciarOcr = async () => {
+  const ocr = await PaddleOCR.create({
+    lang: 'pt',
+    ocr_version: 'PP-OCRv5',
+    ortOptions: {
+      backend: 'wasm',
+      wasmPaths: '/wasm/',
+      numThreads: 2,
+      simd: true
     }
-  }
-};
+  })
+
+  return ocr;
+}
