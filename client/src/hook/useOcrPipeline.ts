932d105b66462d8e6211f7d041453062fd311f2d
deservs <viniciuscapela6014@gmail.com>
2026-09-23 22:46:17 -0300

    ajustes no useOcrPipeline, quase finalizado. Código para a ação do que fazer durante a leitura semi-pronto

--- client/src/hook/useOcrPipeline.ts
+++ client/src/hook/useOcrPipeline.ts
@@ -1,7 +1,9 @@
+import { PdfpageToImage } from '../utils/pdfConvert';
 import { useCallback, useEffect, useRef, useState } from 'react'
 
 type StatusProcesso = 'idle' | 'carregando' | 'sucesso' | 'erro';
 
+export default function useOcrPipeline() {
 const [status, setStatus] = useState<StatusProcesso>('idle');
 
 const workerRef = useRef<Worker | null>(null);
@@ -23,12 +25,38 @@ useEffect(() => {
     };
 }, [finalizarWorker]);
 
-const executarTarefa = useCallback(async (dados: ArrayBuffer) =>{
+const executarTarefa = useCallback(async (dados: File) =>{
     //impedir que o usuario inicie mais de um worker
     finalizarWorker();
     setStatus('carregando');
+    const image = await PdfpageToImage(dados, 1);
+    const imgBuffer = image.data.buffer;
+    const imgwidth = image.width;
+    const imgheight = image.height;
 
     const worker = new Worker(
-        new URL('../workers/ocr.worker.ts', import.meta.url)
-    )
-});
+        new URL('../workers/ocr.worker.ts', import.meta.url),
+        { type: 'module' }
+    );
+    workerRef.current = worker;
+
+    worker.onmessage = (event) => {
+        if (event.data.status === 'sucesso') {
+            setStatus('sucesso');
+        }
+        if (event.data.status === 'erro') {
+            setStatus('erro');
+        }
+        console.log('Mensagem recebida do worker:', event.data);
+        finalizarWorker();
+    };
+
+    worker.onerror = (error) => {
+        console.error('Erro no worker:', error);
+        setStatus('erro');
+        finalizarWorker();
+    }
+    worker.postMessage({dados}, [dados]);
+},[finalizarWorker]);
+return { status, executarTarefa, finalizarWorker};
+}
